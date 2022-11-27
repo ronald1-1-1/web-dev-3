f37be0e00e3b71bcfd2e55a5989ff3950718ae15
MikhailKazaninWG <mishakazanin@gmail.com>
2022-11-27 16:28:24 +0300

    feat: add styles to navigation item for selected page

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,16 @@ function ready() {
 
     const dif = (endTime.getTime() - startTime.getTime()) / 1000;
     footer.innerHTML += 'Page load time is ' + dif + 'seconds';
+
+    if (document.location.href.includes("/index.html")){
+        console.log("aaaaaaaaa")
+        document.getElementById("index-nav").classList.add("navigation__elem-selected")
+    }
+
+    if (document.location.href.includes("/about.html")){
+        document.getElementById("about-nav").classList.add("navigation__elem-selected")
+    }
 }
 
 document.addEventListener("DOMContentLoaded", ready);
+
